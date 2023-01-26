e19c0ef1d2fa47b1681ae383b7830266b0650a08
mrog9 <mwr2@clemson.edu>
2023-01-26 06:59:17 -0500

    Task 34-Electron App with trak toggle and dashboard buttons

--- Electron/main.js
+++ Electron/main.js
@@ -1,18 +1,41 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow,ipcMain } = require('electron')
 const path = require('path')
 const { menubar } = require('menubar');
+const {execFile}=require('child_process')
 
 
 function createWindow () {
-//   const win = new BrowserWindow({
-//     width: 800,
-//     height: 600,
-//     webPreferences: {
-//       preload: path.join(__dirname, 'preload.js')
-//     }
-//   })
+  const win = new BrowserWindow({
+  width: 800,
+  height: 600,
+  // will create a window without menu
+  frame: false,
+  webPreferences: {
+    preload: path.join(__dirname, 'preload.js')
+  }
+})
 
-  win.loadFile('index.html')
+  win.loadFile('src/index.html')
+
+  // if main.js gets message from src/js/index.js in channel 'close_app'
+
+  ipcMain.on('close_app', (event, data)=>{
+    win.close()
+  })
+}
+
+// another function that creates a window, but instead loads src/dashboard.html
+
+const createDashboard = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+   
+  win.loadFile('src//dashboard.html')
 }
 
 app.whenReady().then(() => {
@@ -35,4 +58,21 @@ const mb = menubar();
 mb.on('ready', () => {
   console.log('app is ready');
   // your app code here
+
+// if main.js receives message from src/js/index.js in channel 'msg' then the function
+// createDashboard() runs
+
+  ipcMain.on("msg", (event, data)=>{
+    if (data === "dash") createDashboard()
+  })
+
+  // if main.js receives message from src/js/index.js in channel 'trak', then will run an app to delete/create file on os
+
+  ipcMain.on('trak', (event, data)=>{
+    execFile('windowsService//bin//Debug//net7.0//windowsService.exe')
+
+  // for windowsService app, downloaded .net core sdk
+})
+
+
 });
