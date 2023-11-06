84847c8504278bb30373e4a9e91cfb99acfdd320
ai4he <106041678+ai4he@users.noreply.github.com>
2023-11-06 10:52:21 -0500

    Update modelweb.py

--- AI_Modeling/llm/modelweb.py
+++ AI_Modeling/llm/modelweb.py
@@ -21,9 +21,8 @@ GET_ACTIVITIES: You will get from me a list of the latests activities executed o
 GIVE_REFLECTION: You will always create a brief very short plan(50 words) to achieve the goal.
 GIVE_SCHEDULE: You will always create a brief schedule for the next three (3) minutes. The schedule must be diplayed using HTML tags. The HTML table will contain the following fields Start_Time, End_Time, and Activity.
 GET_NEW_ACTIVITIES: You will get from me the activities that I executed after your suggestions.
-GIVE_DECISION: You must only respond one of these two options; STATUS_RETURN (and then jump to GIVE_REFLECTION) or STATUS_FINISH (and then jump to END_SESSION).
+GIVE_DECISION: You must only respond one of these two options; STATUS_RETURN (and then jump to GIVE_REFLECTION).
 ... (this GIVE_REFLECTION/GIVE_SCHEDULE/GET_NEW_ACTIVITIES/GIVE_DECISION can repeat N times)
-END_SESSION: You will finish once you detect progress towards the goal.
 
 Begin!
 """
