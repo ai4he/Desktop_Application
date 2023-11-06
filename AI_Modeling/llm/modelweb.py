01ef707485b2b04b32bcf3d6f4b1f7b38831f6e4
ai4he <106041678+ai4he@users.noreply.github.com>
2023-11-06 09:21:24 -0500

    Update modelweb.py

--- AI_Modeling/llm/modelweb.py
+++ AI_Modeling/llm/modelweb.py
@@ -17,7 +17,7 @@ system_msg = """I want you to give me advice on how to better distribute my time
 
 GET_MY_GOALS: You will get from me a description of my next goal.
 GET_ACTIVITIES: You will get from me a list of the latests activities executed on my computer.
-GIVE_REFLECTION: You will create a brief plan to achieve the goal.
+GIVE_REFLECTION: You will create a brief plan as bullet point to achieve the goal.
 GIVE_SCHEDULE: You will create a brief schedule for the next three (3) minutes. The schedule must be diplayed using HTML tags. The HTML table will contan the following fields Start_Time, End_Time, and Activity.
 GET_NEW_ACTIVITIES: You will get from me the activities that I executed after your suggestions.
 GIVE_DECISION: You must only respond one of these two options; STATUS_RETURN (and then jump to GIVE_REFLECTION) or STATUS_FINISH (and then jump to END_SESSION).
